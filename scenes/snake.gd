8174ca926ebef93ca6bd5ba6dea3af4398ac4d29
conor-wilson <conorwilson1998@gmail.com>
2024-04-01 13:03:26 +1100

    Added set_tail_cell() func to Snake scene script

--- scenes/snake.gd
+++ scenes/snake.gd
@@ -3,9 +3,9 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_head_cell(Vector2(11, 10), Vector2.UP)
+	set_head_cell(Vector2(11, 10), Vector2.RIGHT)
 	set_cell(1, Vector2(10, 10), 0, Vector2(4,0), 2)
-	set_cell(1, Vector2(9,  10), 0, Vector2(5,1), 2)
+	set_tail_cell(Vector2(9, 14), Vector2.RIGHT)
 
 
 # TODO: Descriptor
@@ -18,8 +18,14 @@ func set_head_cell(coords: Vector2i, direction: Vector2):
 	set_cell(1, coords, 0, Vector2(3,0), altID)
 
 
-#func set_tail_cell(coords: Vector2i, direction: Vector2):
+# TODO: Descriptor
+func set_tail_cell(coords: Vector2i, direction: Vector2):
 
+	# Obtain the alternativeTileID from the priovided direction var
+	var altID = cardinal_to_alt_id(direction)
+	
+	# Set the appropriate cell to the tail tile
+	set_cell(1, coords, 0, Vector2(5,1), altID)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
