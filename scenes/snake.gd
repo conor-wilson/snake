aef9aacf75c2c11bf0adec90e400c0fadbde6607
conor-wilson <conorwilson1998@gmail.com>
2024-04-01 13:12:09 +1100

    Added set_straight_cell() func to Snake scene script

--- scenes/snake.gd
+++ scenes/snake.gd
@@ -4,27 +4,25 @@ extends TileMap
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_head_cell(Vector2(11, 10), Vector2.RIGHT)
-	set_cell(1, Vector2(10, 10), 0, Vector2(4,0), 2)
-	set_tail_cell(Vector2(9, 14), Vector2.RIGHT)
+	set_straight_cell(Vector2(10, 10), Vector2.RIGHT)
+	set_tail_cell(Vector2(9, 10), Vector2.RIGHT)
 
 
 # TODO: Descriptor
 func set_head_cell(coords: Vector2i, direction: Vector2):
-	
-	# Obtain the alternativeTileID from the priovided direction var
 	var altID = cardinal_to_alt_id(direction)
-	
-	# Set the appropriate cell to the head tile
 	set_cell(1, coords, 0, Vector2(3,0), altID)
 
 
 # TODO: Descriptor
-func set_tail_cell(coords: Vector2i, direction: Vector2):
-
-	# Obtain the alternativeTileID from the priovided direction var
+func set_straight_cell(coords: Vector2i, direction: Vector2):
 	var altID = cardinal_to_alt_id(direction)
-	
-	# Set the appropriate cell to the tail tile
+	set_cell(1, coords, 0, Vector2(4,0), (altID-1)%2+1)
+
+
+# TODO: Descriptor
+func set_tail_cell(coords: Vector2i, direction: Vector2):
+	var altID = cardinal_to_alt_id(direction)
 	set_cell(1, coords, 0, Vector2(5,1), altID)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
