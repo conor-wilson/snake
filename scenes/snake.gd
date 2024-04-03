d85b5bd9ce281fc19bd677318ff7995726c8d485
conor-wilson <conorwilson1998@gmail.com>
2024-04-04 00:18:35 +1100

    Reordered funcs and variables within snake.gd to make a bit more sense

--- scenes/snake.gd
+++ scenes/snake.gd
@@ -1,41 +1,19 @@
 extends TileMap
 
-
-## ----- ADAPTED FROM OLD Player NODE SCRIPT ----- ##
-## TODO: Review these features as they likely need to be renamed or adjusted
-
-# The cardinal direction of the snake's head
-var direction : Vector2 = Vector2.RIGHT
-
-# update_direction_from_input (TODO)
-func update_direction_from_input():
-	# TODO: Fix bug where the player can go 180 degrees by quickly pressing 2 buttons.
-	if Input.is_action_pressed("move_up") && direction != Vector2.DOWN:
-		direction = Vector2.UP
-	if Input.is_action_pressed("move_right") && direction != Vector2.LEFT:
-		direction = Vector2.RIGHT
-	if Input.is_action_pressed("move_down") && direction != Vector2.UP:
-		direction = Vector2.DOWN
-	if Input.is_action_pressed("move_left") && direction != Vector2.RIGHT:
-		direction = Vector2.LEFT
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	update_direction_from_input()
-
-## ----------------------------------------------- ##
-
-
-var snakeTiles : Array
+var snakeTiles : Array   # The array of coordinates that the snake occupies
+var direction  : Vector2 # The cardinal direction of the snake's head
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
 	# Set the snake and apple to their starting positions
+	direction = Vector2.RIGHT
 	snakeTiles = [Vector2i(11, 10), Vector2i(10, 10), Vector2i(9, 10)]
 	renderNewSnake()
 	set_apple_cell()
 
+
+## ------------- Snake Rendering Functions ------------- ##
 
 # TODO: Descriptor
 # TODO: Change the direction vectors to not always be Vector2.RIGHT
@@ -52,7 +30,6 @@ func renderNewSnake():
 	# Render tail
 	set_tail_cell(snakeTiles[len-1], Vector2.RIGHT)
 
-
 # TODO: Descriptor
 # TODO: Change the direction vectors to not always be Vector2.RIGHT
 func renderSnakeUpdate(oldTailCoords: Vector2i = Vector2i(-1,-1)):
@@ -61,6 +38,28 @@ func renderSnakeUpdate(oldTailCoords: Vector2i = Vector2i(-1,-1)):
 	erase_cell(1, oldTailCoords)
 	set_tail_cell(snakeTiles[snakeTiles.size()-1], Vector2.RIGHT)
 
+
+## ------------- Snake Movement Functions -------------- ##
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	update_direction_from_input()
+
+# TODO: Descriptor
+func update_direction_from_input():
+	# TODO: Fix bug where the player can go 180 degrees by quickly pressing 2 buttons.
+	if Input.is_action_pressed("move_up") && direction != Vector2.DOWN:
+		direction = Vector2.UP
+	if Input.is_action_pressed("move_right") && direction != Vector2.LEFT:
+		direction = Vector2.RIGHT
+	if Input.is_action_pressed("move_down") && direction != Vector2.UP:
+		direction = Vector2.DOWN
+	if Input.is_action_pressed("move_left") && direction != Vector2.RIGHT:
+		direction = Vector2.LEFT
+
+# TODO: Descriptor
+func _on_ticker_timeout():
+	moveSnake()
 
 # TODO: Descriptor
 func moveSnake(): 
@@ -81,11 +80,12 @@ func moveSnake():
 	renderSnakeUpdate(oldTailCoords)
 
 
+## --------------- Cell Setter Functions --------------- ##
+
 # TODO: Descriptor
 func set_head_cell(coords: Vector2i, direction: Vector2):
 	var altID = cardinal_to_alt_id(direction)
 	set_cell(1, coords, 0, Vector2(3,0), altID)
-
 
 # TODO: Descriptor
 func set_body_cell(coords: Vector2i, dir1: Vector2, dir2: Vector2): 
@@ -100,19 +100,16 @@ func set_body_cell(coords: Vector2i, dir1: Vector2, dir2: Vector2):
 		var altID = cardinal_to_corner_alt_id(vSum)
 		set_cell(1, coords, 0, Vector2(5,0), altID)
 
-
 # TODO: Descriptor
 func set_tail_cell(coords: Vector2i, direction: Vector2):
 	var altID = cardinal_to_alt_id(direction)
 	set_cell(1, coords, 0, Vector2(5,1), altID)
-
 
 # TODO: Descriptor
 func set_apple_cell(): 
 	# TODO: make sure apple doesn't spawn on snake tile
 	var coords = Vector2i(randi_range(2,17), randi_range(2,17))
 	set_cell(1, coords, 0, Vector2(2,1))
-
 
 # TODO: Descriptor
 func cardinal_to_alt_id(direction: Vector2) -> int:
@@ -131,9 +128,9 @@ func cardinal_to_alt_id(direction: Vector2) -> int:
 
 	return altID
 
-
 # TODO: Descriptor
 func cardinal_to_corner_alt_id(direction: Vector2) -> int:
+
 	var altID = 0
 	if direction == Vector2.UP + Vector2.RIGHT: 
 		altID = 1
@@ -147,8 +144,3 @@ func cardinal_to_corner_alt_id(direction: Vector2) -> int:
 		print_debug("invalid direction input to cardinal_to_corner_alt_id():", direction)
 	
 	return altID
-
-
-# TODO: Descriptor
-func _on_ticker_timeout():
-	moveSnake()
