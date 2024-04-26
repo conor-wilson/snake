0d129367b60664e87ba7c850903ab569a428ab7a
conor-wilson <conorwilson1998@gmail.com>
2024-04-27 01:22:57 +1000

    Fixed up visuals of HUD a bit

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -5,23 +5,31 @@ signal start_game
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$StartButton.hide()
-	$DeathMessage.hide()
+	$Message.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func show_start_menu():
+	# Message behaviour
+	$Message.text = "Welcome to Snake!"
+	$Message.show()
+	# Button behaviour
 	$StartButton.show()
-	$DeathMessage.hide()
 
 func show_game_over_screen():
+	# Message behaviour
+	$Message.text = "Game Over"
+	$Message.show()
+	# Button behaviour
 	$StartButton.hide()
-	$DeathMessage.show()
 	
 func show_in_game_hud():
+	# Message behaviour
+	$Message.hide()
+	# Button behaviour
 	$StartButton.hide()
-	$DeathMessage.hide()
 
 func _on_start_button_pressed():
 	start_game.emit()
