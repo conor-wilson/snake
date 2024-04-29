37a08da0b8cefdbfa91abfa696865f243e4f5e87
conor-wilson <conorwilson1998@gmail.com>
2024-04-29 22:49:37 +1000

    Added Score lable to HUD

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -6,6 +6,7 @@ signal start_game
 func _ready():
 	$StartButton.hide()
 	$Message.hide()
+	$Score.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -18,6 +19,8 @@ func show_start_menu():
 	# Button behaviour
 	$StartButton.text = "START GAME"
 	$StartButton.show()
+	# Score behaviour
+	$Score.hide()
 
 func show_pause_menu():
 	# Message behaviour
@@ -26,6 +29,8 @@ func show_pause_menu():
 	# Button behaviour
 	$StartButton.text = "RESUME"
 	$StartButton.show()
+	# Score behaviour
+	$Score.show()
 
 func show_game_over_screen():
 	# Message behaviour
@@ -34,12 +39,16 @@ func show_game_over_screen():
 	# Button behaviour
 	$StartButton.text = "TRY AGAIN"
 	$StartButton.show()
+	# Score behaviour
+	$Score.hide()
 
 func show_in_game_hud():
 	# Message behaviour
 	$Message.hide()
 	# Button behaviour
 	$StartButton.hide()
+	# Score behaviour
+	$Score.show()
 
 func _on_start_button_pressed():
 	start_game.emit()
