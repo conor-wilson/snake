8ddd62762fb1d0617b13e97893ebf7d2a0c8d198
conor-wilson <conorwilson1998@gmail.com>
2024-05-19 17:08:22 +1000

    Tidied up HUD a bit

--- scenes/HUD.gd
+++ scenes/HUD.gd
@@ -1,12 +1,39 @@
 extends CanvasLayer
 
+@export var max_num_digits:int = 3
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hide()
 
 func update_score(score: int):
-	$Score.text = "Score: 00" + str(score)
+	$Score.text = "Score: " + get_zeros(score) + str(score)
 
 func update_high_score(high_score: int):
-	$HighScore.text = "High Score: 0" + str(high_score)
+	$HighScore.text = "High Score: " + get_zeros(high_score) + str(high_score)
 
+
+# TODO: Descriptor
+func get_zeros(score:int) -> String:
+	# Find the number of digits in the provided score
+	var num_zeros:int = max_num_digits-num_digits(score)
+	
+	# Return a string of zeros to be prepended to that score
+	var output:String = ""
+	for x in num_zeros:
+		output += "0"
+	return output
+
+# TODO: Descriptor
+func num_digits(n:int) -> int :
+	
+	# If the provided number is 0, it's one digit long
+	if n == 0:
+		return 1
+	
+	# Otherwise, calculate the number of digits and return
+	var digits:int = 0
+	while n!=0:
+		n /= 10
+		digits += 1
+	return digits
