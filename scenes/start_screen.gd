dd270dc355b817fc3ae6a8c9b6ed7dcb91de7a39
conor-wilson <conorwilson1998@gmail.com>
2024-07-19 00:17:07 +1000

    Refactored start screen title to change to WORM in WORM MODE

--- scenes/start_screen.gd
+++ scenes/start_screen.gd
@@ -1,17 +1,24 @@
 extends Control
 
+var worm_title:Texture  = load("res://art/WormTitle.png")
+var snake_title:Texture = load("res://art/Title.png") 
+
 func show_and_focus():
 	show()
 	$PressAnyKey/Timer.start()
 
+# update_mode (TODO)
+# TODO: This is almost an exact copy of Snake.update_mode(). Fix this.
 func update_mode():
 	
 	# Set the TileMap source ID
 	var source_id : int
 	if Global.worm_mode:
 		source_id = 1
+		$Title.texture = worm_title
 	else:
 		source_id = 0
+		$Title.texture = snake_title
 	
 	# Refresh all the cells on all layers
 	var layer_cell_coords = $TileMap.get_used_cells(0)
