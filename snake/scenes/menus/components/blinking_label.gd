281a9c10c18fd352cba1b4ac674143dfbe81c488
conor-wilson <conorwilson1998@gmail.com>
2024-10-05 01:53:15 +1000

    Tidied up menu component script files

--- snake/scenes/menus/components/blinking_label.gd
+++ snake/scenes/menus/components/blinking_label.gd
@@ -1,13 +1,13 @@
 extends Label
 
-@export var active:bool
-@export var number_of_blinks:int
-@export var blink_duration:float
-@export var time_between_blinks:float
-@export var vertical_speed:float
-@export var horizontal_speed:float
+@export var active:bool               # If true, the blinking-label will enter the tree activated.
+@export var number_of_blinks:int      # The number of blinks that occur when using temporarily_activate().
+@export var blink_duration:float      # The duration that the label is shown during a blink.
+@export var time_between_blinks:float # The duration that the label is hidden between blinks.
+@export var vertical_speed:float      # The speed at which the label will float vertically while blinking.
+@export var horizontal_speed:float    # The speed at which the label will float horizontally while blinking.
 
-var lifetime:float = 0
+var lifetime:float # How long the label stays active until deactivating when using temporarily_activate().
 
 func _ready():
 	lifetime = number_of_blinks*(blink_duration+time_between_blinks)
@@ -20,6 +20,7 @@ func _process(delta):
 	if active:
 		position += Vector2(horizontal_speed, vertical_speed) * delta
 
+# activate activates the blinking label, optionally placing it at the provided coordinates.
 func activate(coords:Vector2 = Vector2(-1,-1)):
 	
 	if coords.x >= 0 && coords.y >= 0:
@@ -28,17 +29,22 @@ func activate(coords:Vector2 = Vector2(-1,-1)):
 	active = true
 	start_blinking()
 
+# disable disables the blinking label, and hides it.
 func disable():
 	active = false
 	$ShowTimer.stop()
 	$HideTimer.stop()
 	hide()
-	
+
+# temporarily_activate temporarily activates the blinking label at the provided screen
+# coordinates.
 func temporarily_activate(coords:Vector2):
 	activate(coords)
 	await get_tree().create_timer(lifetime).timeout
 	disable()
 
+# start_blinking starts the blinking process by starting both timers, and offsetting
+# their start times so that the configured timing-settings are applied.
 func start_blinking():
 	show()
 	$ShowTimer.start(blink_duration+time_between_blinks)
--- snake/scenes/menus/components/menu_button.gd
+++ snake/scenes/menus/components/menu_button.gd
@@ -4,41 +4,54 @@ extends Button
 var unchecked_icon = preload("res://scenes/menus/components/checkBoxUnchecked.png")
 var checked_icon = preload("res://scenes/menus/components/checkBoxChecked.png")
 
-# TODO: Descriptor
-@export var checkable : bool
-@export var invert_mute_behavior : bool
+@export var checkable : bool            # If true, the button will include a check-box next to it.
+@export var invert_mute_behavior : bool # If true, the button will make noise when the game is muted and visa versa (useful when this is the mute button itself).
 
-# TODO: Descriptor
 enum MenuSound {NOTHING, START_GAME, SELECT, CONFIRM, CANCEL}
-@export var focus_sound   : MenuSound = MenuSound.SELECT
-@export var pressed_sound : MenuSound = MenuSound.CONFIRM
+@export var focus_sound   : MenuSound = MenuSound.SELECT  # The sound that is emitted when the MenuButton grabs focus.
+@export var pressed_sound : MenuSound = MenuSound.CONFIRM # The sound that is emitted when the menuButton is pressed.
 
-var skip_next_focus_sound : bool = false
+var skip_next_focus_sound : bool = false # If true, the next time this button grabs focus, it will not emit the focus noise.
 
 func _ready(): 
 	if checkable: 
 		icon = checked_icon
 
+# focus grabs focus to the MenuButton, skipping the focus sound if skip_sound is true.
 func focus(skip_sound:bool=false):
 	self.skip_next_focus_sound = skip_sound
 	grab_focus()
 
-# TODO: Descriptor
+# update_icon updates the button's check-box icon according to the provided checked bool
+# (as long as this is a checkable MenuButton).
+func update_icon(checked:bool): 
+	if checkable: 
+		icon = get_checkbox_icon(checked)
+	else:
+		print_debug("update_icon() was called on non-checkable menu button:", checked)
+
+# get_checkbox_icon returns either the checked_icon resource or the unchecked_icon
+# resorce depending on the provided value of checked.
+func get_checkbox_icon(checked:bool) -> Resource:
+	if checked:
+		return checked_icon
+	else:
+		return unchecked_icon
+
+
+## --------------- Sound-Emmitting Funcs --------------- ##
+
 func _on_focus_entered():
 	if !skip_next_focus_sound && !Global.mute:
 		play_sound(focus_sound)
 	else: 
 		skip_next_focus_sound = false
 
-# TODO: Descriptor
 func _on_pressed():
-	
-	# Play the desired audio
 	if Global.mute == invert_mute_behavior:
 		play_sound(pressed_sound)
 
-
-# TODO: Descriptor
+# play_sound plays the provided MenuSound.
 func play_sound(sound:MenuSound):
 	match sound:
 		MenuSound.NOTHING:
@@ -51,18 +64,3 @@ func play_sound(sound:MenuSound):
 			$CancelSound.play()
 		MenuSound.START_GAME:
 			$StartGameSound.play()
-
-# TODO: Descriptor
-# TODO: Maybe use inheritance for this instead of this...
-func update_icon(checked:bool): 
-	if checkable: 
-		icon = get_checkbox_icon(checked)
-	else:
-		print_debug("update_icon() was called on non-checkable menu button:", checked)
-
-# TODO: Descriptor
-func get_checkbox_icon(checked:bool) -> Resource:
-	if checked:
-		return checked_icon
-	else:
-		return unchecked_icon
